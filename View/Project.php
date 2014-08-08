5f230f16e7a8a756d5700ac197f3b312cd357b8c
Johan <johan.preynat@gmail.com>
2014-08-08 22:56:01 +0000

    Updating project page

--- View/Project.php
+++ View/Project.php
@@ -22,22 +22,80 @@
     <!-- #################### GENERAL INFORMATIONS ################### -->
 
     <div class="row">
-        <h3>Lieu</h3>
+        <div class="col-sm-5">
+            
+            <h4 class="page-header">Lieu</h4>
         
-        <?php if (strlen($project['p_city']) > 0): ?>
-        <h4><?= "{$project['p_city']}, {$project['country']}" ?></h4>
+            <?php if (strlen($project['p_city']) > 0): ?>
+            <p><?= "{$project['p_city']}, {$project['country']}" ?></p>
         
-        <?php else: ?>
-        <h4><?= $project['country'] ?></h4>
+            <?php else: ?>
+            <p><?= $project['country'] ?></p>
+            
+            <?php endif; ?>
+            
+        </div>
         
-        <?php endif; ?>
+        <div class="col-sm-5 col-sm-offset-2">
+            
+            <h4 class="page-header">Thème</h4>
+            <p><?= $project['theme'] ?></p>
+            
+        </div>
     </div>
     
+    <!-- #################### PROJECTS INFORMATIONS ################### -->
     
     <div class="row">
-        <h3>Description du projet</h3>
+        <h4 class="page-header">Projets</h4>
         <p><?= $project['p_proj'] ?></p>
     </div>
+    
+    <div class="row">
+        <h4 class="page-header">Résumé des actions</h4>
+        <p><?= $project['p_summ'] ?></p>
+    </div>
+    
+    <div class="row">
+        <h4 class="page-header">Bénéficiaires</h4>
+        <p><?= $project['p_bene'] ?></p>
+    </div>
+    
+    <!-- #################### CONTACT INFORMATIONS ################### -->
+    
+    <div class="row">
+        <div class="col-sm-6 col-sm-offset-1">
+        
+            <h4 class="page-header">Contact</h4>
+            
+            <p><?= "{$project['c_first']} {$project['c_last']}" ?></p>
+            
+            <?php if(strlen($project['c_street']) > 0) : ?>
+            <p><?= $project['c_street'] ?></p>
+            <?php endif; ?>
+            
+            <?php if(strlen($project['c_city']) > 0) : ?>
+            <p>
+                <?php if(strlen($project['c_postal']) > 0) : ?>
+                <?= "{$project['c_street']} " ?>
+                <?php endif; ?>
+            <?= "{$project['c_city']} " ?>
+            </p>
+            <?php endif; ?>
+            
+            <?php if(strlen($project['c_country']) > 0) : ?>
+            <p><?= $project['c_country'] ?></p>
+            <?php endif; ?>
+            
+            <p><?= $project['c_mail'] ?></p>
+            
+            <?php if(strlen($project['c_phone']) > 0) : ?>
+            <p><?= $project['c_phone'] ?></p>
+            <?php endif; ?>
+            
+        </div>
+    </div>
+    
 </article>
 
 <?php $contents = ob_get_clean(); ?>
