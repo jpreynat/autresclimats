f4879a5e9b16c60fdbbe34274c4f51053ce94ad3
Johan <johan.preynat@gmail.com>
2014-08-08 15:10:01 +0000

    Added responsive for phone

--- View/template.php
+++ View/template.php
@@ -2,6 +2,7 @@
 <html lang="fr">
     <head>
         <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
         <title><?= $page_title ?></title>
         
         <link href="autresclimats.ico" rel="shortcut icon" type="image/x-icon">
