6e7582b20916d2ffe7b3342706c32f6ad8594a67
Johan <johan.preynat@gmail.com>
2014-08-08 15:46:06 +0000

    Updated query for project and project display

--- Model/Model.php
+++ Model/Model.php
@@ -97,7 +97,7 @@ function getProject($project_id) {
               INNER JOIN countries ON projects.coun_id = countries.coun_id
               INNER JOIN themes ON projects.t_id = themes.t_id
               INNER JOIN ' . $q1 . ' ON projects.c_id = Q1.c_id
-              WHERE p_id = ' . $project_id;
+              WHERE p_id = ?';
     
     /*
     $query = 'SELECT proj_id AS id, proj_name AS name, coun_name_fr AS country, proj_language AS language,'
