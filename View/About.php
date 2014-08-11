33d707ed8346be3294c513826175565449d46658
Johan <johan.preynat@gmail.com>
2014-08-11 12:53:12 +0000

    Added about and contact pages

---
+++ View/About.php
@@ -0,0 +1,15 @@
+<?php $page_title = "A propos d'Autres Climats"; ?>
+
+<?php ob_start(); ?>
+
+<header>
+</header>
+
+<article>
+</article>
+
+<?php $contents = ob_get_clean(); ?>
+
+<?php $javascript = ''; ?>
+
+<?php require 'template.php'; ?>
---
+++ View/Contact.php
@@ -0,0 +1,15 @@
+<?php $page_title = 'Pour nous contacter...'; ?>
+
+<?php ob_start(); ?>
+
+<header>
+</header>
+
+<article>
+</article>
+
+<?php $contents = ob_get_clean(); ?>
+
+<?php $javascript = ''; ?>
+
+<?php require 'template.php'; ?>
