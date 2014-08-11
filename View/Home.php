02f2566c84cde657f354a1da8cd2d56c0c4428e9
Johan <johan.preynat@gmail.com>
2014-08-11 13:31:38 +0000

    Added about and contact pages

--- View/Home.php
+++ View/Home.php
@@ -1,14 +1,24 @@
 <?php $page_title = 'Bienvenue sur Autres Climats'; ?>
 
+
+
 <?php ob_start(); ?>
 
+
 <header>
+
     <div class="jumbotron">
+
         <h1>Bienvenue !</h1>
+
         <p>Bienvenue sur le site d'Autres Climats.</p>
+
         <p>Venez rejoindre nos <?= $nbProjects ?> projets !</p>
+
     </div>
+
 </header>
+
 
 <article>
 
@@ -42,6 +52,4 @@
 
 <?php $contents = ob_get_clean(); ?>
 
-?php $javascri<pt = ''; ?>
-
-<?php require 'template.php'; ?>
+<?php 
