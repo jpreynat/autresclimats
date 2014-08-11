0a734837c8ce73562b7923fcf35ac4e50ac03d6a
Johan <johan.preynat@gmail.com>
2014-08-11 09:06:37 +0000

    Began last projects on home page

--- View/Home.php
+++ View/Home.php
@@ -13,24 +13,27 @@
 <article>
 </article>
 
+
+<h4 class="page-header">Derniers projets</h4>
+
 <div class="row">
     
 <?php foreach ($projects as $project): ?>
 <?php if (!$project['p_logo'])
         $project['p_logo'] = 'js/holder.js/200x200'; ?>
 
-    <div class="col-md-3 text-center">
+    <div class="col-md-4 text-center">
         <div class="img-container">
-                <div class="row img-thumbnail">
-                    <a href="<?= "index.php?action=project&id=" . $project['p_id'] ?>">
-                        <img src="<?= $project['p_logo'] ?>" alt="..." class="img-pres"/>
-                    </a>
-                </div>
-                <div class="row">
-                    <label class="proj-name"><?= $project['p_name'] ?></label>
-                </div>
-                <div class="row empty">
-                </div>
+            <div class="row img-thumbnail">
+                <a href="<?= "index.php?action=project&id=" . $project['p_id'] ?>">
+                    <img src="<?= $project['p_logo'] ?>" alt="..." class="img-pres"/>
+                </a>
+            </div>
+        </div>
+        <div class="row">
+            <label class="proj-name"><?= $project['p_name'] ?></label>
+        </div>
+        <div class="row empty">
         </div>
     </div>
 
