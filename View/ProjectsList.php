039fba0aaabe83bc8de7d1caff1e68f5e7b04c69
Johan <johan.preynat@gmail.com>
2014-08-11 14:25:31 +0000

    Updated projects list middle display

--- View/ProjectsList.php
+++ View/ProjectsList.php
@@ -12,18 +12,20 @@
 
     <div class="col-md-3 text-center">
         <div class="img-container">
-                <div class="row img-thumbnail">
-                    <a href="<?= "index.php?action=project&id=" . $project['p_id'] ?>">
-                        <img src="<?= $project['p_logo'] ?>" alt="..." class="img-pres"/>
-                    </a>
-                </div>
-                <div class="row">
-                    <label class="proj-name"><?= $project['p_name'] ?></label>
-                </div>
-                <div class="row empty">
-                </div>
+        
+            <div class="row img-thumbnail">
+                <a href="<?= "index.php?action=project&id=" . $project['p_id'] ?>">
+                    <img src="<?= $project['p_logo'] ?>" alt="..." class="img-pres"/>
+                </a>
+            </div>
+                
         </div>
-    </div>
+        <div class="row">
+            <label class="proj-name"><?= $project['p_name'] ?></label>
+        </div>
+        <div class="row empty">
+        </div>
+</div>
 
 <?php endforeach; ?>
     
