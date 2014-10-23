eb883c201a1d3be98ef2d6f19e115d945366dc90
Johan <johan.preynat@gmail.com>
2014-10-23 15:13:52 +0000

    project location info ORDER BY country, city

--- Model/Model.php
+++ Model/Model.php
@@ -465,7 +465,7 @@ function projectsLocationService() {
     $projectsInfo = [];
     
     foreach ($result as $projectInfo) {
-        $projectsInfo[$projectInfo['name']] = [$projectInfo['city'], $projectInfo['country']];
+        $projectsInfo[utf8_encode($projectInfo['name'])] = [utf8_encode($projectInfo['city']), utf8_encode($projectInfo['country'])];
     }
     
     return $projectsInfo;
