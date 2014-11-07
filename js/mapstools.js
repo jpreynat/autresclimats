4f808876b41ed0ad083a041dbec4b4e02c568755
Johan <johan.preynat@gmail.com>
2014-11-07 12:03:59 +0000

    setTimeout codeAddress

--- js/mapstools.js
+++ js/mapstools.js
@@ -55,7 +55,7 @@ function codeAddress(address, projects) {
         globalIW.open(map, marker);
       });
     } else {
-      alert("Geocode failed, status=" + status);
+      //alert("Geocode failed, status=" + status);
       setTimeout(codeAddress(address, projects), 500);
     }
   });
